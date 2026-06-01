dc8848912cd2b8c6c1d6bd23b6e7a8c8662d89fe
rominiki <rominanikasa1996@gmail.com>
2026-06-01 16:13:47 +0200

    removed unused import

--- src/test/java/com/rominiki/waytohome/controller/HealthControllerTest.java
+++ src/test/java/com/rominiki/waytohome/controller/HealthControllerTest.java
@@ -3,7 +3,6 @@ package com.rominiki.waytohome.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import com.rominiki.waytohome.config.SecurityConfig;
