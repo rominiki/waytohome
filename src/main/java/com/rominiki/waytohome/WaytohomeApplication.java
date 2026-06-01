6c04ba12db9faecf5d9348fc20fb04e43b795836
rominiki <rominanikasa1996@gmail.com>
2026-06-01 14:48:33 +0200

    initial commit

---
+++ src/main/java/com/rominiki/waytohome/WaytohomeApplication.java
@@ -0,0 +1,13 @@
+package com.rominiki.waytohome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaytohomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaytohomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rominiki/waytohome/WaytohomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rominiki.waytohome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaytohomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
